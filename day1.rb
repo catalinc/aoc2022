45f86111ac01fa05900d294398ca57d8b7238b15
Catalin Cristu <catalin.cristu@gmail.com>
2022-12-01 12:35:49 +0200

    Add solution to day 1

---
+++ day1.rb
@@ -0,0 +1,12 @@
+inventory = []
+
+IO.foreach("input/day1.txt", "") {|paragraph| inventory << paragraph.split.map {|x| x.to_i}}
+
+calories_per_elf = inventory.map {|foods| foods.sum}
+
+# part 1
+puts calories_per_elf.max
+
+# part 2
+calories_per_elf.sort! {|x,y| y <=> x}
+puts calories_per_elf[0, 3].sum
